55f11a86522558170e158832dec4a3889a16edcf
xiaohui <xiaohui@zhangxh.net>
2015-11-18 16:20:34 +0800

    Add "Sheryl" => "Sherry" equivalence

--- lib/fullname/equivalence.rb
+++ lib/fullname/equivalence.rb
@@ -1081,6 +1081,7 @@ module Fullname
     Sheridan                      	Dan
     Sheridan                      	Danny
     Sheridan                      	Sher
+    Sheryl                          Sherry
     Shirley                       	Lee
     Shirley                       	Sherry
     Shirley                       	Shirl
